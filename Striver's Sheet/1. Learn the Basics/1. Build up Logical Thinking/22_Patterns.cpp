5cca4c75e7583553d262983bd72f72d657fc4977
Aditya <adityapatekar001@gmail.com>
2026-05-22 20:49:12 +0530

    Fix: Remove nested repository and reorganize folder structure

--- Striver's Sheet/1. Learn the Basics/1. Build up Logical Thinking/22_Patterns.cpp
+++ Striver's Sheet/1. Learn the Basics/1. Build up Logical Thinking/22_Patterns.cpp
@@ -288,15 +288,241 @@ void pattern10(int n) {
     }
 }
 
+// Binary Left to Right Triangle
+
+/*
+
+1
+01
+101
+0101
+10101
+
+*/
+
+// Code
 void pattern11(int n) {
+    int start = 1;
+    for(int i=0; i<n; i++) {
+        if (i % 2 == 0) start = 1;
+        else start = 0;
+        for(int j=0; j<=i; j++) {
+            cout << start;
+            start = 1 - start;
+        }
+        cout << endl;
+    }
 }
 
+// Left to Right lower and Upper Triangle
+
+/*
+
+1        1
+12      21
+123    321
+1234  4321
+1234554321
+
+*/
+
+// Code
+void pattern12(int n) {
+    int spaces = 2*(n-1);
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=i; j++) {
+            cout << j;
+        }
+
+        for(int j=1; j<=spaces; j++) {
+            cout << " ";
+        }
+
+        for (int j=i; j>=1; j--) {
+            cout << j;
+        }
+
+        cout << endl;
+
+        spaces -= 2;
+    }
+}
+
+// Numeric left to Right (1 to 15) Triangle
+
+/*
+
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15
+
+*/
+
+// Code
+void pattern13(int n) {
+    int count = 1;
+    for(int i=1; i<=n; i++) {
+        for(int j=0; j<i; j++) {
+            cout << count << " ";
+            count += 1;
+        }
+        cout << endl;
+    }
+}
+
+// Alphabetic Left to Right Triangle
+
+/* 
+
+A 
+A B 
+A B C 
+A B C D 
+A B C D E
+
+*/
+
+// Code
+void pattern14(int n) {
+    for(int i=1; i<=n; i++) {
+        for(char ch='A'; ch<'A'+i; ch++) {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+// Reverse Alphabetic Right to Left Triangle
+
+/*
+
+A B C D E 
+A B C D 
+A B C 
+A B 
+A
+
+*/
+
+//Code 
+void pattern15(int n) {
+    for(int i=1; i<=n; i++) {
+        for(char ch='A'; ch<=n-i+'A'; ch++) {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+// Alphabetic Left to Right Triangle (with similar Alphabets in each row)
+
+/*
+
+A 
+B B 
+C C C 
+D D D D 
+E E E E E
+
+*/
+
+// Code
+void pattern16(int n) {
+    for(int i=0; i<n; i++) {
+        char ch = 'A' + i;
+        for(int j=0; j<=i; j++) {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+// Aplhabetic Triangle
+
+/*
+
+    A    
+   ABA   
+  ABCBA  
+ ABCDCBA
+
+*/
+
+// Code
+void pattern17(int n) {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n-1-i; j++) {
+            cout << " ";
+        }
+
+        char ch='A';
+        int breakpoint = (2*i+1) / 2;
+        for(int j=0; j<=2*i+1; j++) {
+            cout << ch;
+            if(j<=breakpoint) ch++;
+            else ch--;
+        }
+
+        for(int j=0; j<n-1-i; j++) {
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+// Reverse Alphabetical Order Left to Right Triangle
+
+/*
+
+E
+ED
+EDC
+EDCB
+EDCBA
+
+*/
+
+// Code
+void pattern18(int n) {
+    for(int i=0; i<=n; i++) {
+        char ch = 'A' + n;
+        for(int j=0; j<=i; j++) {
+            cout << ch;
+            ch--;
+        }
+        cout << endl;
+    }
+}
+
+// Space Traingle
+
+/*
+
+
+
+*/
+
+// Code
+// void pattern19(int n) {
+//     for(int i=0; i<n; i++) {
+//         for(int j=0; j<=i; j++) {
+//             cout << "*";
+//         }
+
+//         for(int j=0; j)
+//         cout << endl;
+//     }
+// }
+
 int main() {
-    int n = 5;
+    int n = 4;
 
     // cout << "Enter the Number: ";
     // cin >> n;
 
-    pattern11(n);
+    pattern18(n);
 
 }
