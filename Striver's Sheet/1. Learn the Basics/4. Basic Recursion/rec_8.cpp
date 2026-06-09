d844ef2aaa02bbe9a0d007d18be1697abca7ad56
Aditya <adityapatekar001@gmail.com>
2026-06-09 22:30:24 +0530

    Fibonacci Series

---
+++ Striver's Sheet/1. Learn the Basics/4. Basic Recursion/rec_8.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+    public:
+    void fibonacci(int n) {
+        if (n == 0) {
+            cout << "The Fibonacci Series up to " << n << "th term:" << endl;
+            cout << 0;
+        } else {
+            int secondLast = 0;
+            int last = 1;
+
+            cout << "The Fibonacci Series up to " << n << "th term:" << endl;
+            cout << secondLast << " " << last << " ";
+            
+            int cur;
+            for (int i=2; i <= n; i++) {
+                cur = last + secondLast;
+                secondLast = last;
+                last = cur;
+                cout << cur << " ";
+            }
+        }
+    }
+};
+
+int main() {
+    int n;
+
+    cout << "Enter the number: ";
+    cin >> n;
+
+    Solution S1;
+    S1.fibonacci(n);
+
+    return 0;
+}
