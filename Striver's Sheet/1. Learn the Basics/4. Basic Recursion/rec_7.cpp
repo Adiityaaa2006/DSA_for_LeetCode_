212d9e92990f965825dcbd85dfb4d4faa49c9497
Aditya <adityapatekar001@gmail.com>
2026-06-11 19:57:46 +0530

    LeetCode - 125. Valid Palindrome

--- Striver's Sheet/1. Learn the Basics/4. Basic Recursion/rec_7.cpp
+++ Striver's Sheet/1. Learn the Basics/4. Basic Recursion/rec_7.cpp
@@ -3,21 +3,31 @@ using namespace std;
 
 class Solution {
     public:
-    bool palindrome(int n, string& str) {
-        if (n >= str.length() / 2) return true;
+        // LeetCode
+    // 125. Valid Palindrome
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.size();
 
-        if (str[n] != str[str.length() - 1 - n]) return false;
+        while (i <= j) {
+            while(i < j && !isalnum(s[i])) i++;  // isalnum - contains only alphabetic and numeric values
+            while(i < j && !isalnum(s[j])) j--;
+            if (i < j && tolower(s[i]) != tolower(s[j])) return false;  // tolwer - convert uppercase alphabets to lowercase
+            i++;
+            j--;
+        }
 
-        return palindrome(n+1, str);
+        return true;
     }
 };
 
 int main() {
-    string s = "abcba";
+    string str = "A man, a plan, a canal: Panama";
+
     Solution sol;
 
-    cout << sol.palindrome(0, s);
-    cout << endl;
+    bool result = sol.isPalindrome(str);
+    cout << "Output: " << result;
 
     return 0;
 }
