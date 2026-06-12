9e77e2b929d153103e647cc406097407ce9ffebd
Aditya <adityapatekar001@gmail.com>
2026-06-12 23:59:41 +0530

    Hash Map PB 1

---
+++ Striver's Sheet/1. Learn the Basics/5. Basic Hashing/hashMap1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+    public:
+    void hashMap() {
+        vector<int> arr;
+        int n = arr.size();
+
+        int hash[n] = {0};
+
+    }
+};
