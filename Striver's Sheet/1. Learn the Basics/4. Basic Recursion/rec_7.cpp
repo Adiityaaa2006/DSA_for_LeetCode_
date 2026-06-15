ea700b1bc5f0ee8ef1d15a15c5c5d08b3caf2574
Aditya <adityapatekar001@gmail.com>
2026-06-15 22:43:23 +0530

    LeetCode 125. Valid Palindrome

--- Striver's Sheet/1. Learn the Basics/4. Basic Recursion/rec_7.cpp
+++ Striver's Sheet/1. Learn the Basics/4. Basic Recursion/rec_7.cpp
@@ -7,7 +7,7 @@ class Solution {
     // 125. Valid Palindrome
     bool isPalindrome(string s) {
         int i = 0;
-        int j = s.size();
+        int j = s.size()-1;
 
         while (i <= j) {
             while(i < j && !isalnum(s[i])) i++;  // isalnum - contains only alphabetic and numeric values
