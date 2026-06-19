5390b2fbbb8098adebe1a3293f12b1354173c724
Aditya <adityapatekar001@gmail.com>
2026-06-19 21:25:49 +0530

    Bubble Sort

--- Striver's Sheet/2. Sorting Techinques/1. Sorting I/bubbleSort.cpp
+++ Striver's Sheet/2. Sorting Techinques/1. Sorting I/bubbleSort.cpp
@@ -5,22 +5,35 @@ using namespace std;
 class Solution {
     public:
     void bubbleSort(vector<int>& arr, int n) {
-
+        // Finds the largest element and sort it.
         for (int i=n-1; i>=0; i--) {
-
+            /*
+            Loop running in an reverse order so that 
+            every time the highest element gets sorted,
+            second loop will run till the ith index which 
+            exclude the highest element index & saves time.
+            */
             bool swapped = false;
 
             // 13, 46, 24, 52, 20, 9
             for (int j=0; j<i; j++) {
                 if (arr[j] > arr[j+1]) { 
+                    /*
+                    If element at j is greater than j+1 then,
+                    swap them. 
+                    */
                     swap(arr[j], arr[j+1]);
-                    // int temp = arr[j];
-                    // arr[j] = arr[j+1];
-                    // arr[j+1] = temp;
+                    swapped = true;
+                    // If swap has happened then define it as true
                 }
             }
 
             if (!swapped) break;
+            /*
+            If the array is not swapped this means 
+            the array is sorted. There is noo need to iterate further,
+            this breaks the loop and exists which saves time.
+            */
         }
 
         cout << "After Bubble Sort: " << endl;
