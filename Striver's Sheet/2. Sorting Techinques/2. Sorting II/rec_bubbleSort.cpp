f8b82e35eeab73b11f5f1efe5fbfeb5b3840006c
Aditya <adityapatekar001@gmail.com>
2026-06-19 22:59:49 +0530

    Recursive Bubble Sort

---
+++ Striver's Sheet/2. Sorting Techinques/2. Sorting II/rec_bubbleSort.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+    public:
+    void rec_bubbleSort(vector<int>& arr, int n) {
+        // cout << "N Value: " << n << endl;
+        if (n < 0) return;
+
+        int i = 0;
+        while(i < n) {
+            // cout << "While Start i=" << i << " ";
+            if (arr[i] > arr[i+1]) {  // Sort Array in Ascending Order - Lower to Greater
+                swap(arr[i], arr[i+1]);  
+                // cout << "Swap " << i << " ";
+            }
+            i++;
+        }
+        // cout << "While END " << endl;
+
+        rec_bubbleSort(arr, n-1);
+    }
+};
+
+int main() {
+    vector<int> arr = {13, 46, 24, 52, 20, 9};
+    int n = arr.size();
+
+    cout << "Before Bubble Sorting: \n";
+    for (int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    Solution S1;
+    S1.rec_bubbleSort(arr, n);
+
+    cout << "After Bubble Sort: " << endl;
+    for (int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
